f1b0a7e82c7349914b56e9daf973f65aebdbe7a0
Mosbycr <mosbycr@gmail.com>
2019-03-04 14:06:13 -0500

    complete js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,61 @@ var loseScore = 0;
 var scoreCounter = 0;
 
 var randomNumberGenerated = Math.floor(Math.random() * (102)) + 19;
-console.log(randomNumberGenerated);
 $("#randomNumber").text(randomNumberGenerated);
+console.log(randomNumberGenerated);
 
 var image1 = Math.floor(Math.random() * 12) + 1;
 var image2 = Math.floor(Math.random() * 12) + 1;
 var image3 = Math.floor(Math.random() * 12) + 1;
 var image4 = Math.floor(Math.random() * 12) + 1;
-console.log(image1);
-console.log(image2);
-console.log(image3);
-console.log(image4);
+console.log(image1, image2, image3, image4);
+
+function reset(){
+    scoreCounter = 0;
+    randomNumberGenerated = Math.floor(Math.random() * (102)) + 19;
+    image1 = Math.floor(Math.random() * 12) + 1;
+    image2 = Math.floor(Math.random() * 12) + 1;
+    image3 = Math.floor(Math.random() * 12) + 1;
+    image4 = Math.floor(Math.random() * 12) + 1;
+
+    $("#randomNumber").text(randomNumberGenerated);
+    $("#counter").text(scoreCounter);
+}
+
+function winLose(){
+    if(scoreCounter === randomNumberGenerated){
+        winScore++;
+        $("#wins").text(winScore);
+        reset();
+    } else if(scoreCounter > randomNumberGenerated){
+        loseScore++;
+        $("#losses").text(loseScore);
+        reset();
+    }
+}
 
 $("#image-1").on("click", function(){
     scoreCounter = scoreCounter + image1;
-    console.log(scoreCounter);
+    $("#counter").text(scoreCounter);
+    winLose();
 });
 
 $("#image-2").on("click", function(){
     scoreCounter = scoreCounter + image2;
-    console.log(scoreCounter);
+    $("#counter").text(scoreCounter);
+    winLose();
 });
 
 $("#image-3").on("click", function () {
     scoreCounter = scoreCounter + image3;
-    console.log(scoreCounter);
+    $("#counter").text(scoreCounter);
+    winLose();
 });
 
 $("#image-4").on("click", function () {
     scoreCounter = scoreCounter + image4;
-    console.log(scoreCounter);
+    $("#counter").text(scoreCounter);
+    winLose();
 });
 
 
