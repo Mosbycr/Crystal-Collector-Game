da4aa0bdfaa0332984499021483b31803c630220
Mosbycr <mosbycr@gmail.com>
2019-03-04 13:42:29 -0500

    variables set for each image and on click set individually. could get onclick for all to work but not sure how to add each value seperately

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,55 @@
 var winScore = 0;
 var loseScore = 0;
 var scoreCounter = 0;
-var randomNumber = Math.floor(Math.random() * (102)) + 19;
-console.log(randomNumber);
-var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log(crystalNumbers);
 
-$(".crystalImages").on('click', function (event) {
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    alert ("yess");
-        
-    var crystalValue = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)]; 
-    scoreCounter = scoreCounter + crystalValue;
-    
-    $("#counter").text = scoreCounter;
+var randomNumberGenerated = Math.floor(Math.random() * (102)) + 19;
+console.log(randomNumberGenerated);
+$("#randomNumber").text(randomNumberGenerated);
+
+var image1 = Math.floor(Math.random() * 12) + 1;
+var image2 = Math.floor(Math.random() * 12) + 1;
+var image3 = Math.floor(Math.random() * 12) + 1;
+var image4 = Math.floor(Math.random() * 12) + 1;
+console.log(image1);
+console.log(image2);
+console.log(image3);
+console.log(image4);
+
+$("#image-1").on("click", function(){
+    scoreCounter = scoreCounter + image1;
     console.log(scoreCounter);
-});  
+});
 
-//}
-// images are creating numbers but are random every click instead of staying same value
+$("#image-2").on("click", function(){
+    scoreCounter = scoreCounter + image2;
+    console.log(scoreCounter);
+});
+
+$("#image-3").on("click", function () {
+    scoreCounter = scoreCounter + image3;
+    console.log(scoreCounter);
+});
+
+$("#image-4").on("click", function () {
+    scoreCounter = scoreCounter + image4;
+    console.log(scoreCounter);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".crystalImages").on('click', function (event) {
+//     event.stopPropagation();
+//     event.stopImmediatePropagation();
+//     alert ("yess");
+
+// });  
+
